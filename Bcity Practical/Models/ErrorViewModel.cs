819aa7a86dde7edb43be5492e376e5586cbd8b24
Oscar Ramaposa <106546756+Oscar-Ramaphosa@users.noreply.github.com>
2026-02-15 14:49:55 +0200

    Add files via upload

---
+++ Bcity Practical/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Bcity_Practical.Models
+{
+    public class ErrorViewModel
+    {
+        // Stores the request ID for debugging purposes
+        public string? RequestId { get; set; }
+
+        // Indicates whether the RequestId should be shown
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Bcity Practical/Services/IClientCodeGeneratorService.cs
@@ -0,0 +1,8 @@
+﻿namespace Bcity_Practical.Services
+{
+    public interface IClientCodeGeneratorService
+    {
+        //this Generates a unique client code based on the client name
+        Task<string> GenerateClientCodeAsync(string clientName);
+    }
+}
